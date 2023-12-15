9012e19a7d22a00e63c3a0f435c1826b1e14b278
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-15 10:54:54 -0700

    Finish and pass tests for problems: 7 & 8

--- problems/p7.ts
+++ problems/p7.ts
@@ -1,4 +1,12 @@
 import { prisma } from "./prisma";
 
 // get average score for a user
-export const getAverageScoreForUser = async (userId: number) => {};
+export const getAverageScoreForUser = async (userId: number) => {
+    const ratings = await prisma.starRating.findMany({
+        where: {
+            userId: userId
+        }
+    });
+    const scores = ratings.map((rating) => rating.score);
+    return scores.reduce((a, b) => a + b, 0) / scores.length;
+};
--- problems/p8.ts
+++ problems/p8.ts
@@ -1,10 +1,30 @@
-import { maxBy, minBy } from "remeda";
+import { maxBy, minBy, groupBy } from "remeda";
 import { prisma } from "./prisma";
 
 // Always tell truths, don't you ever lie, to solve this problem, just try a `groupBy`
 
 // find the critic with the lowest average score
-export const findTheGrumpiestCriticId = async () => {};
+export const findTheGrumpiestCriticId = async () => {
+    const ratings = await prisma.starRating.findMany();
+    const groupedRatings = groupBy(ratings, (rating) => rating.userId);
+    const averages = Object.entries(groupedRatings).map(([userId, ratings]) => {
+        const scores = ratings.map((rating) => rating.score);
+        const average = scores.reduce((a, b) => a + b, 0) / scores.length;
+        return { userId: Number(userId), average };
+    });
+    const grumpiest = minBy(averages, (average) => average.average);
+    return grumpiest?.userId;
+};
 
 // find the critic with the highest average score
-export const findTheNicestCriticId = async () => {};
+export const findTheNicestCriticId = async () => {
+    const ratings = await prisma.starRating.findMany();
+    const groupedRatings = groupBy(ratings, (rating) => rating.userId);
+    const averages = Object.entries(groupedRatings).map(([userId, ratings]) => {
+        const scores = ratings.map((rating) => rating.score);
+        const average = scores.reduce((a, b) => a + b, 0) / scores.length;
+        return { userId: Number(userId), average };
+    });
+    const nicest = maxBy(averages, (average) => average.average);
+    return nicest?.userId;
+};
