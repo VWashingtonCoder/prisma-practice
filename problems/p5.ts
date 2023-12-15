3cb13f6f966815d2f55c41d02d5eaa84c8a69bea
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-15 10:38:08 -0700

    Finish and pass tests for p5, 6, & 11

--- problems/p5.ts
+++ problems/p5.ts
@@ -1,54 +1,25 @@
-import { groupBy, map, reduce, sumBy } from "remeda";
+import { sumBy } from "remeda";
 import { prisma } from "./prisma";
-import { StarRating } from "@prisma/client";
+import { Movie } from "@prisma/client";
 
-// hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
+// hint: find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
 export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
-    // const movies = await prisma.movie.findMany({
-    //     include: {
-    //     starRatings: true,
-    //     },
-    // });
+  const movies = await prisma.movie.findMany({
+    include: {
+      starRatings: true,
+    },
+  });
 
-    const starRatings = await prisma.starRating.findMany({
-        include: {
-        movie: true,
-        },
-    });
+  const moviesWithAverageScore: Movie[] = [];
 
-    const movies = groupBy(starRatings, (star) => star.movieId);
+  movies.forEach((movie) => {
+    const { starRatings, ...rest } = movie;
+    const averageScore =
+      sumBy(starRatings, (star) => star.score) / starRatings.length;
 
-    console.log(movies);
-    // const result = map(movies, (movie) => {
-    //     const averageScore = sumBy(movie, (star) => star.score) / movie.length;
-    //     const ratings = movie.filter((star) => star.score > 3);
+    if (averageScore > n) moviesWithAverageScore.push({ ...rest });
+  });
 
-    //     return {
-    //         ...movie,
-    //         averageScore,
-    //         starRatings: ratings,
-    //     };
-    // });
-
-
-    
-    // const moviesWithAverageScore = movies.map((movie) => {
-    //     const averageScore = sumBy(movie.starRatings, (star) => star.score) / movie.starRatings.length;
-    //     const ratings = movie.starRatings.filter((star) => star.score > 3);
-
-    //    const movieWithAverageScore = {
-    //     ...movie,
-    //     averageScore,
-    //     starRatings: ratings,
-    //     };
-
-    //     console.log(movieWithAverageScore);
-    //     return movieWithAverageScore;
-    // });
-    
-    // const result = moviesWithAverageScore.filter((movie) => movie.averageScore > n);
-
-    // console.log(result);
-    // return result;
+  return moviesWithAverageScore;
 };
--- problems/p6.ts
+++ problems/p6.ts
@@ -1,4 +1,15 @@
 import { prisma } from "./prisma";
 
 // find all movies that a user has watched
-export const findAllMoviesThatAUserWatched = async (userId: number) => {};
+export const findAllMoviesThatAUserWatched = async (userId: number) => {
+  const ratings = await prisma.starRating.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      movie: true,
+    },
+  });
+
+  return ratings.map((rating) => rating.movie);
+};
