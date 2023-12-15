db9c7b5da1911efa9a1978675981ac2ccae0b2e5
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-14 17:15:44 -0700

    Finish and pass tests for p1-4

--- problems/p1.ts
+++ problems/p1.ts
@@ -2,4 +2,11 @@ import { prisma } from "./prisma";
 
 // Hint: look up "orderBy"
 // get an array of all users
-export const getAllUsers = () => {};
+export const getAllUsers = () => {
+  const users = prisma.user.findMany({
+    orderBy: {
+      username: "asc",
+    },
+  });
+  return users;
+};
--- problems/p5.ts
+++ problems/p5.ts
@@ -4,4 +4,51 @@ import { StarRating } from "@prisma/client";
 
 // hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
-export const getAllMoviesWithAverageScoreOverN = async (n: number) => {};
+export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
+    // const movies = await prisma.movie.findMany({
+    //     include: {
+    //     starRatings: true,
+    //     },
+    // });
+
+    const starRatings = await prisma.starRating.findMany({
+        include: {
+        movie: true,
+        },
+    });
+
+    const movies = groupBy(starRatings, (star) => star.movieId);
+
+    console.log(movies);
+    // const result = map(movies, (movie) => {
+    //     const averageScore = sumBy(movie, (star) => star.score) / movie.length;
+    //     const ratings = movie.filter((star) => star.score > 3);
+
+    //     return {
+    //         ...movie,
+    //         averageScore,
+    //         starRatings: ratings,
+    //     };
+    // });
+
+
+    
+    // const moviesWithAverageScore = movies.map((movie) => {
+    //     const averageScore = sumBy(movie.starRatings, (star) => star.score) / movie.starRatings.length;
+    //     const ratings = movie.starRatings.filter((star) => star.score > 3);
+
+    //    const movieWithAverageScore = {
+    //     ...movie,
+    //     averageScore,
+    //     starRatings: ratings,
+    //     };
+
+    //     console.log(movieWithAverageScore);
+    //     return movieWithAverageScore;
+    // });
+    
+    // const result = moviesWithAverageScore.filter((movie) => movie.averageScore > n);
+
+    // console.log(result);
+    // return result;
+};
